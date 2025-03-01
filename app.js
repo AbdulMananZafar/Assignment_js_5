96bf5c6435682cfc1053f81af41939fa53d2e754
AbdulMananZafar <abdul.manan.zafar.awan24@gmail.com>
2025-03-01 10:52:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+// CHAPTER NO. 17-20
+
+// Question No.1
+// var arr =[];
+
+// Question No.2
+// var matrix = [
+//     [0 1 2 3  "<br />"],
+//     [1, 0, 1, 2 + "<br />"],
+//     [2, 1, 0, 1]
+//   ];
+// var matrix = [
+//     [1,2,3]
+//     [4,5,6]
+//     [7,8,9]
+//   ];
+//   document.write(matrix)
+
+// var matrix = [
+//     [0, 1, 2, 3, 4],
+//     [5, 6, 7, 8, 9]
+//   ];
+// document.write(matrix)  
+
+// Question No.3
+// for(var i=1;i<=10;i++){
+//     document.write(i ,"<br>")}
+
+// Question No.4
+// var user_table =prompt("Enter the Table number :");
+// var user_length = prompt("What is the length you want :");
+//     document.write("Multiplication table of " , user_table , "<br>");
+//     document.write("Length " , user_length , "<br>")
+// for (i=1 ; i<=user_length; i++){
+//     document.write(user_table,"*",i , "=" , user_table * i ,"<br>");
+// }
+
+// Question No.5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (var i = 0; i < fruits.length; i++) {
+//     document.write(fruits[i], "<br>" )
+// }
+// document.write("Element at index 0 is " , fruits[0] ,"<br>" ,"Element at index 1 is " , fruits[1] ,"<br>",  "Element at index 2 is " , fruits[2] , "<br>", "Element at index 3 is " ,fruits[3] ,"<br>", "Element at index 4 is " , fruits[4]  );
+
+// Question No.6
+// document.write("Counting : ")
+// for (var i = 1; i <= 15; i++) {
+//     document.write( i , "," , "<br>" );
+// }
+// document.write("Reverse : ")
+// for (var i = 15; i >= 1; i--) {  
+//       document.write( i , "," );
+// }
+// document.write("Even : ")
+// for (var i = 0; i <= 20; i += 2) {  
+//       document.write(i , ",")
+// }
+
+// document.write("Odd : ")
+// for (var i = 1; i <= 20; i += 2) {
+//     document.write(i , ",") }
+
+document.write("Series : ")
+for (var i = 2; i <= 20; i += 2) {  
+            document.write(i , "k,") }
+    
+
+
+
+  
+// Question No.7
+// Question No.8
+// Question No.9
+// Question No.10
+
